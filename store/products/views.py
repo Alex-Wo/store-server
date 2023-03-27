df8813830ee909397f4916ab6a4e2f6145e8cc4f
Mr. Freeman <blackdevops13@gmail.com>
2023-03-27 16:21:05 +0300

    Initial commit

---
+++ store/products/views.py
@@ -0,0 +1,13 @@
+from django.shortcuts import render
+
+
+def index(request):
+    context = {'title': 'Store'}
+    return render(request, 'products/index.html', context)
+
+
+def products(request):
+    context = {
+        'title': 'Store - Каталог',
+    }
+    return render(request, 'products/products.html', context)
