097759c23616ca91af4d0ae39b710faf64b04faa
Mr. Freeman <blackdevops13@gmail.com>
2023-03-28 19:40:59 +0300

    Add tags & placeholders

--- store/products/views.py
+++ store/products/views.py
@@ -1,13 +1,19 @@
 from django.shortcuts import render
+from products.models import ProductCategory, Product
 
 
 def index(request):
-    context = {'title': 'Store'}
+    context = {
+        'title': 'Store',
+        'is_promotion': False
+    }
     return render(request, 'products/index.html', context)
 
 
 def products(request):
     context = {
         'title': 'Store - Каталог',
+        'products': Product.objects.all(),
+        'categories': ProductCategory.objects.all(),
     }
     return render(request, 'products/products.html', context)
